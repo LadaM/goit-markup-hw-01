4e9261f9740d0847cf707257e6718ed35193500e
Lada <lada11lada@gmail.com>
2024-03-14 19:59:11 +0100

    HW4 -- modal styling and logic

---
+++ modal.js
@@ -0,0 +1,16 @@
+(() => {
+  const refs = {
+    openModalBtn: document.querySelector("[data-modal-open]"),
+    closeModalBtn: document.querySelector("[data-modal-close]"),
+    modal: document.querySelector("[data-modal]"),
+    modalBackdrop: document.querySelector("[data-modal-backdrop]"),
+  };
+
+  refs.openModalBtn.addEventListener("click", toggleModal);
+  refs.closeModalBtn.addEventListener("click", toggleModal);
+
+  function toggleModal() {
+    refs.modal.classList.toggle("is-hidden");
+    refs.modalBackdrop.classList.toggle("is-hidden");
+  }
+})();
